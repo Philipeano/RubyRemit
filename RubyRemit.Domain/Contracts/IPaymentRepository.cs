be7f15f752b2edcb48941f1317439e441947be8a
Philip Newman <philipeano@gmail.com>
2021-02-27 12:47:22 +0100

    Define interfaces for UoW and repos

---
+++ RubyRemit.Domain/Contracts/IPaymentRepository.cs
@@ -0,0 +1,12 @@
+﻿using RubyRemit.Domain.Entities;
+using RubyRemit.Domain.Interfaces;
+using System;
+
+namespace RubyRemit.Domain.Contracts
+{
+    public interface IPaymentRepository : IRepository<Payment>
+    {
+        Payment AddPayment(string cardNumber, string cardHolder, DateTime expiryDate, string securityCode,
+                           decimal amount, DateTime dateInitiated);
+    }
+}
---
+++ RubyRemit.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using RubyRemit.Domain.Entities;
+using System;
+
+namespace RubyRemit.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IRepository<Payment> PaymentRepository { get; }
+
+        public IRepository<PaymentState> PaymentStateRepository { get; }
+
+
+        public void CommitChanges();
+
+
+        public void RejectChanges();
+
+
+        public new void Dispose();
+    }
+}
