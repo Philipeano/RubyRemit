f66605541debcfd93fb9b248f6d3d3cb2184a75c
Philip Newman <philipeano@gmail.com>
2021-03-03 10:28:22 +0100

    Define IPaymentRepository interface

---
+++ RubyRemit.Domain/Interfaces/IPaymentRepository.cs
@@ -0,0 +1,22 @@
+﻿using RubyRemit.Domain.Entities;
+using RubyRemit.Domain.LookUp;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RubyRemit.Domain.Interfaces
+{
+    public interface IPaymentRepository : IGenericRepository<Payment>
+    {
+        public PaymentStateEnum GetCurrentStatus(long paymentId);
+
+
+        public int GetNumberOfAttempts(long paymentId);
+
+
+        public IQueryable<Payment> GetAllPayments(Expression<Func<Payment, bool>> expression);
+
+
+        public Payment GetPaymentById(long id);
+    } 
+}
