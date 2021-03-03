de094d63c1cdb709b123b9a7a52f372e62b3e0fc
Philip Newman <philipeano@gmail.com>
2021-03-03 15:57:31 +0100

    Implement UnitOfWork class

---
+++ RubyRemit.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RubyRemit.Domain.Entities;
+using RubyRemit.Domain.Interfaces;
+using System.Linq;
+
+namespace RubyRemit.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RubyRemitContext _dbContext;
+
+        public IGenericRepository<Payment> PaymentRepository => new GenericRepository<Payment>(_dbContext);
+
+        public IGenericRepository<PaymentState> PaymentStateRepository => new GenericRepository<PaymentState>(_dbContext);
+
+
+
+        public UnitOfWork(RubyRemitContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+
+        public void RejectChanges()
+        {
+            if (_dbContext.ChangeTracker.HasChanges())
+            {
+                var modifiedEntities = _dbContext.ChangeTracker.Entries()
+                    .Where(e => e.State != EntityState.Unchanged);
+
+                foreach(var entity in modifiedEntities)
+                {
+                    if (entity.State == EntityState.Added)
+                        entity.State = EntityState.Detached;
+                    else
+                        entity.Reload();
+                }
+            }
+        }
+    }
+}
