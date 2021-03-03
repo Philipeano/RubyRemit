5adb27c54927a8c42eb1585c87b7d4f5b1e1c9c7
Philip Newman <philipeano@gmail.com>
2021-03-03 15:20:58 +0100

    Implement GenericRepository class for shared CRUD operations

---
+++ RubyRemit.Infrastructure/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RubyRemit.Domain.Interfaces;
+
+namespace RubyRemit.Infrastructure
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly RubyRemitContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        protected DbSet<T> DbSet => _dbSet ?? _dbContext.Set<T>();
+
+
+        public GenericRepository(RubyRemitContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+
+        public T Add(T entity)
+        {
+            DbSet.Add(entity);
+            return entity;
+        }
+    }
+}
