4205692e5e3b9b6fb55e08a26ff63ae0f9332676
Philip Newman <philipeano@gmail.com>
2021-03-08 00:16:17 +0100

    Refactor PaymentsController to depend only on IOrchestrator

--- RubyRemit.API/Controllers/PaymentsController.cs
+++ RubyRemit.API/Controllers/PaymentsController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RubyRemit.Business.Contracts;
 using RubyRemit.Domain.DTOs;
 using RubyRemit.Domain.Entities;
 using RubyRemit.Domain.Interfaces;
 using RubyRemit.Domain.LookUp;
 using RubyRemit.Infrastructure.PaymentGateways.Contracts;
-using RubyRemit.Infrastructure.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,64 +18,37 @@ namespace RubyRemit.Api.Controllers
     [ApiController]
     public class PaymentsController : ControllerBase
     {
+        private readonly IOrchestrator _orchestrator;
 
-        private readonly IPaymentGateway _paymentService;
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly Validator _validator = new Validator();
-
-        public PaymentsController(IPaymentGateway gateway, IUnitOfWork unitOfWork)
+        public PaymentsController(IOrchestrator orchestrator)
         {
-            _paymentService = gateway;
-            _unitOfWork = unitOfWork;
+            _orchestrator = orchestrator;
         }
 
 
         [HttpPost]
-        public ActionResult ProcessPayment([FromBody] PaymentRequestBody payment)
+        public async Task<ActionResult> ProcessPaymentAsync([FromBody] PaymentRequestBody payment)
         {
             try
             {
-
-                // Define rules for determining which gateway to use
-
-                bool gatewayResult = _paymentService.ProcessTransaction(payment.CreditCardNumber, payment.CardHolder,
-                    payment.ExpirationDate.ToString(), payment.SecurityCode, payment.Amount.ToString(),
-                    out string message);
-
-                if (gatewayResult == false)
+                bool validationResult = _orchestrator.ValidateUserInput(payment, out string validationMessage);
+                if (!validationResult)
                 {
-                    return BadRequest(JsonSerializer.Serialize(new { message }, typeof(object)));
+                    return BadRequest(JsonSerializer.Serialize(new { validationMessage }, typeof(object)));
                 }
-                else
+
+                bool processingResult = await _orchestrator.ConsumePaymentService();
+                if (!processingResult)
                 {
-                    Payment newPayment = new Payment()
-                    {
-                        CreditCardNumber = payment.CreditCardNumber,
-                        CardHolder = payment.CardHolder,
-                        ExpirationDate = payment.ExpirationDate,
-                        SecurityCode = payment.SecurityCode,
-                        Amount = payment.Amount
-                    };
-
-                    PaymentState paymentState = new PaymentState()
-                    {
-                        Payment = newPayment,
-                        State = PaymentStateEnum.Processed,
-                        DateAttempted = DateTime.Now,
-                        Gateway = _paymentService.ServiceName,
-                        Remark = message
-                    };
-
-                    _unitOfWork.PaymentRepository.Add(newPayment);
-                    _unitOfWork.PaymentStateRepository.Add(paymentState);
-                    _unitOfWork.Commit();
-
-                    return Ok(JsonSerializer.Serialize(new { message, data = newPayment }, typeof(object)));
+                    return StatusCode(StatusCodes.Status500InternalServerError, 
+                        JsonSerializer.Serialize(new { message = "Unable to process the payment at this time. Please try again." }, 
+                        typeof(object)));
                 }
+
+                return StatusCode(StatusCodes.Status200OK, JsonSerializer.Serialize(new { message = "Payment processed successfully." }, typeof(object)));                
             }
             catch (Exception ex)
             {
-                _unitOfWork.RejectChanges();
                 return StatusCode(StatusCodes.Status500InternalServerError, JsonSerializer.Serialize(new { message = ex.Message }, typeof(object)));
             }
         }
