ea977d915b3ed33b4326ab2bfba92b729b825112
Philip Newman <philipeano@gmail.com>
2021-03-07 23:41:43 +0100

    Define IOrchestrator interface

---
+++ RubyRemit.Business/Contracts/IOrchestrator.cs
@@ -0,0 +1,16 @@
+﻿using RubyRemit.Domain.DTOs;
+using System.Threading.Tasks;
+
+namespace RubyRemit.Business.Contracts
+{
+    public interface IOrchestrator
+    {
+        public bool ValidateUserInput(PaymentRequestBody paymentInfo, out string validationMessage);
+
+
+        public void ConfigureProcessingRules(decimal amount);
+
+
+        public Task<bool> ConsumePaymentService();
+    }
+}
