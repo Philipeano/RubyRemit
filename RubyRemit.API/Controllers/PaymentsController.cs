a1837bf4a6ce2d23c08c3b1e4d644ee83411c042
Philip Newman <philipeano@gmail.com>
2021-03-08 08:30:01 +0100

    Add XML comments to PaymentsController

--- RubyRemit.API/Controllers/PaymentsController.cs
+++ RubyRemit.API/Controllers/PaymentsController.cs
@@ -2,13 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RubyRemit.Business.Contracts;
 using RubyRemit.Domain.DTOs;
-using RubyRemit.Domain.Entities;
-using RubyRemit.Domain.Interfaces;
-using RubyRemit.Domain.LookUp;
-using RubyRemit.Infrastructure.PaymentGateways.Contracts;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -20,21 +14,34 @@ namespace RubyRemit.Api.Controllers
     {
         private readonly IOrchestrator _orchestrator;
 
+
         public PaymentsController(IOrchestrator orchestrator)
         {
             _orchestrator = orchestrator;
         }
 
 
-        [HttpPost]
-        public async Task<ActionResult> ProcessPaymentAsync([FromBody] PaymentRequestBody payment)
+        // POST: api/payments/processpayment
+        /// <summary>
+        /// Submit a new payment request for processing with an external payment gateway, based on the details in 'paymentRequest'. 
+        /// </summary>
+        /// <param name="paymentRequest">A JSON object containing 'creditCardNumber', 'cardHolder', 'expirationDate', 'securityCode' and 'amount' properties</param>
+        /// <returns>A JSON object containing a string 'message' property.</returns>
+        /// <response code="200">Success! Payment was processed successfully.</response> 
+        /// <response code="400">Bad request! Invalid input detected. See details in response body.</response>
+        /// <response code="500">Error! Unable to process the payment at this time. See details in response body.</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(object))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(object))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(object))]
+        [HttpPost("processpayment")]
+        public async Task<ActionResult> ProcessPaymentAsync([FromBody] PaymentRequestBody paymentRequest)
         {
             try
             {
-                bool validationResult = _orchestrator.ValidateUserInput(payment, out string validationMessage);
+                bool validationResult = _orchestrator.ValidateUserInput(paymentRequest, out string validationMessage);
                 if (!validationResult)
                 {
-                    return BadRequest(JsonSerializer.Serialize(new { validationMessage }, typeof(object)));
+                    return BadRequest(JsonSerializer.Serialize(new { message = validationMessage }, typeof(object)));
                 }
 
                 bool processingResult = await _orchestrator.ConsumePaymentService();
@@ -44,7 +51,6 @@ namespace RubyRemit.Api.Controllers
                         JsonSerializer.Serialize(new { message = "Unable to process the payment at this time. Please try again." }, 
                         typeof(object)));
                 }
-
                 return StatusCode(StatusCodes.Status200OK, JsonSerializer.Serialize(new { message = "Payment processed successfully." }, typeof(object)));                
             }
             catch (Exception ex)
