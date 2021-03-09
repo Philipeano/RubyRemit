6416fcac9cf4a562b9d2d0e5c50c56c39251b2b3
Philip Newman <philipeano@gmail.com>
2021-03-09 10:51:43 +0100

    Refactor PaymentsController to use IOrchestrator

--- RubyRemit.API/Controllers/PaymentsController.cs
+++ RubyRemit.API/Controllers/PaymentsController.cs
@@ -23,10 +23,20 @@ namespace RubyRemit.Api.Controllers
 
         // POST: api/payments/processpayment
         /// <summary>
-        /// Submit a new payment request for processing with an external payment gateway, based on the details in 'paymentRequest'. 
+        /// Submits a new payment request for processing with an external payment gateway, based on the details in <c>paymentRequest</c>. 
         /// </summary>
-        /// <param name="paymentRequest">A JSON object containing 'creditCardNumber', 'cardHolder', 'expirationDate', 'securityCode' and 'amount' properties</param>
-        /// <returns>A JSON object containing a string 'message' property.</returns>
+        /// <param name="paymentRequest">A JSON object containing payment details.</param>
+        /// <remarks>
+        /// The properties of the <c>request</c> parameter are described below: 
+        /// <list type="bullet">
+        /// <item><term>creditCardNumber</term> The credit card number for this transaction</item>
+        /// <item><term>cardHolder</term> The customer name shown on the credit card</item>
+        /// <item><term>expirationDate</term> The expiration date on the credit card</item>
+        /// <item><term>securityCode</term> The 3-digit security code on the credit card, if available</item>
+        /// <item><term>amount</term> The amount to be processed, in British pounds</item>
+        /// </list>
+        /// </remarks>
+        /// <returns>A JSON object containing <c>result</c> (bool), <c>message</c> (string) and <c>data</c> (object) properties.</returns>
         /// <response code="200">Success! Payment was processed successfully.</response> 
         /// <response code="400">Bad request! Invalid input detected. See details in response body.</response>
         /// <response code="500">Error! Unable to process the payment at this time. See details in response body.</response>
@@ -34,7 +44,8 @@ namespace RubyRemit.Api.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(object))]
         [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(object))]
         [HttpPost("processpayment")]
-        public async Task<ActionResult> ProcessPaymentAsync([FromBody] PaymentRequestBody paymentRequest)
+        //public ActionResult<PaymentResponseBody> ProcessPaymentAsync([FromBody] PaymentRequestBody paymentRequest)
+        public async Task<ActionResult<PaymentResponseBody>> ProcessPaymentAsync([FromBody] PaymentRequestBody paymentRequest)
         {
             try
             {
@@ -44,8 +55,8 @@ namespace RubyRemit.Api.Controllers
                     return BadRequest(JsonSerializer.Serialize(new { message = validationMessage }, typeof(object)));
                 }
 
-                bool processingResult = await _orchestrator.ConsumePaymentService();
-                if (!processingResult)
+                GatewayResponse processingResult = await _orchestrator.ConsumePaymentService();
+                if (!processingResult.Succeeded)
                 {
                     return StatusCode(StatusCodes.Status500InternalServerError, 
                         JsonSerializer.Serialize(new { message = "Unable to process the payment at this time. Please try again." }, 
