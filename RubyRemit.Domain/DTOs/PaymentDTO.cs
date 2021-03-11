d22b8fc93dd33ee5f4bf7c5f61bdef9dcd830f78
Philip Newman <philipeano@gmail.com>
2021-03-11 17:19:31 +0100

    Define new PaymentDTO and PaymentStateDTO classes

---
+++ RubyRemit.Domain/DTOs/PaymentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+namespace RubyRemit.Domain.DTOs
+{
+    public class PaymentDTO
+    {
+        public long Id { get; set; }
+
+
+        public string CreditCardNumber { get; set; }
+
+
+        public string CardHolder { get; set; }
+
+
+        public decimal Amount { get; set; }
+
+
+        public virtual ICollection<PaymentStateDTO> ProcessingAttempts { get; set; }
+    }
+}
---
+++ RubyRemit.Domain/DTOs/PaymentStateDTO.cs
@@ -0,0 +1,19 @@
+﻿using RubyRemit.Domain.LookUp;
+using System;
+
+namespace RubyRemit.Domain.DTOs
+{
+    public class PaymentStateDTO
+    {
+        public PaymentStateEnum State { get; set; }
+
+
+        public DateTime DateAttempted { get; set; }
+
+
+        public string Gateway { get; set; }
+
+
+        public string Remark { get; set; }
+    }
+}
