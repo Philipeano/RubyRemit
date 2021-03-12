8c30bbdd43c886f1a20116c2fb328b2a14ce8068
Philip Newman <philipeano@gmail.com>
2021-03-12 17:47:25 +0100

    Configure launch settings for Gateways API

--- RubyRemit.Gateways/Startup.cs
+++ RubyRemit.Gateways/Startup.cs
@@ -1,15 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RubyRemit.Gateways.Services;
+using RubyRemit.Infrastructure.PaymentGateways.Contracts;
 
 namespace RubyRemit.Gateways
 {
@@ -22,11 +19,23 @@ namespace RubyRemit.Gateways
 
         public IConfiguration Configuration { get; }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(setupAction =>
+            {
+                setupAction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status200OK));
+                setupAction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status400BadRequest));
+                setupAction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status500InternalServerError));
+                setupAction.Filters.Add(new ProducesAttribute("application/json"));
+                setupAction.Filters.Add(new ConsumesAttribute("application/json"));
+            });
+
+            services.AddScoped<ICheapPaymentGateway, BasicPaymentService>();
+            services.AddScoped<IExpensivePaymentGateway, PremiumPaymentService>();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -35,12 +44,16 @@ namespace RubyRemit.Gateways
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
